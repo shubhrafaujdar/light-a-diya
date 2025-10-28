09f812e2ef2497e352eabfee906c3278638faf42
Shubhra Singh Faujdar <shubhra.faujdar@gmail.com>
2025-10-27 22:32:24 -0700

    check user session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { UserProvider } from "@/context/UserContext";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { Providers } from "@/components/providers";
 import Navigation from "@/components/Navigation";
+import DirectAuthTest from "@/components/DirectAuthTest";
 import "./globals.css";
 
 const inter = Inter({
@@ -58,6 +59,7 @@ export default function RootLayout({
             <UserProvider>
               <Navigation />
               {children}
+              <DirectAuthTest />
             </UserProvider>
           </LanguageProvider>
         </Providers>
