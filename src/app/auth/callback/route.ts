300ddb0c8251500304ac2420444eabd8c106af70
Shubhra Singh Faujdar <shubhra.faujdar@gmail.com>
2025-12-05 22:26:05 -0800

    fix build

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { cookies } from 'next/headers';
 
+export const runtime = 'nodejs';
+
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
